a08302c80227529ee349345837edb5297ac1cfb4
Bryan White <bryanchriswhite@gmail.com>
2020-11-24 09:52:05 +0100

    add udev setup to install script

--- install.sh
+++ install.sh
@@ -4,12 +4,21 @@ dir=$(dirname "$(realpath $0)")
 files=$(ls $dir/systemd)
 unit_dir=$HOME/.config/systemd/user
 blinkmon_dest=/usr/local/bin/blinkmon
+udev_rules=/etc/udev/rules.d/51-blink1.rules
 
+#TODO: ensure asdf python?
+#TODO: use pipenv instead
 if [[ ! -d ./venv ]]; then
   venv ./venv
 fi
 . ./venv/bin/activate
 pip install -r ./requirements.txt
+
+if [[ ! -f $udev_rules ]]; then
+  echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="27b8", ATTRS{idProduct}=="01ed", MODE:="666", GROUP="plugdev"' | $udev_rules
+  udevadm control --reload
+  udevadm trigger
+fi
 
 if [[ ! -d $unit_dir ]]; then
   mkdir -p $unit_dir
